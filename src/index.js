6f383d90cf4bd4b1cf103774fa1347574307d84c
nateachino <natealleyne88@gmail.com>
2022-11-09 13:53:38 -0500

    Finished home page and added page switching with JavaScript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-// alert('test')
+import { createHeader } from "./header";
+
+const mainContent = document.getElementById("content")
+var page = 12
+
+
+const home = document.getElementById("home-button")
+const menu = document.getElementById("menu-button")
+const about = document.getElementById("about-button")
+
+
+home.addEventListener("click", ()=>{
+    console.log("123")
+    if(page != 1){
+        removeAllChildNodes(mainContent)
+    }
+})
+
+function removeAllChildNodes(nodeList){
+    while (nodeList.firstChild){
+        nodeList.removeChild(nodeList.firstChild);
+    }
+}
