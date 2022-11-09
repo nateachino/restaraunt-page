f5e14857be28cbb800e8b668c7a9f0d06af3a6ca
nateachino <natealleyne88@gmail.com>
2022-11-09 13:53:38 -0500

    Finished home page and added page switching with JavaScript

--- main.js
+++ main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/header.js":
+/*!***********************!*\
+  !*** ./src/header.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createHeader\": () => (/* binding */ createHeader)\n/* harmony export */ });\nconst mainContent = document.getElementById(\"content\")\r\n\r\nfunction createHeader(){\r\n\r\n    const header = document.createElement(\"header\")\r\n    const headerHolder = document.createElement(\"div\")\r\n    const leftHeader = document.createElement(\"div\")\r\n\r\n    const home = document.createElement(\"a\")\r\n    const menu = document.createElement(\"a\")\r\n    const about = document.createElement(\"a\")\r\n\r\n    home.innerHTML = \"Home\"\r\n    menu.innerHTML = \"Menu\"\r\n    about.innerHTML = \"About\"\r\n\r\n    leftHeader.classList.add(\"left-header\")\r\n\r\n    leftHeader.appendChild(home)\r\n    leftHeader.appendChild(menu)\r\n    leftHeader.appendChild(about)\r\n\r\n    headerHolder.appendChild(leftHeader)\r\n    header.appendChild(headerHolder)\r\n\r\n    mainContent.appendChild(header)\r\n\r\n}\r\n\r\n\n\n//# sourceURL=webpack://restaurant-page/./src/header.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("// alert('test')\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./header */ \"./src/header.js\");\n\r\n\r\nconst mainContent = document.getElementById(\"content\")\r\nvar page = 12\r\n\r\n\r\nconst home = document.getElementById(\"home-button\")\r\nconst menu = document.getElementById(\"menu-button\")\r\nconst about = document.getElementById(\"about-button\")\r\n\r\n\r\nhome.addEventListener(\"click\", ()=>{\r\n    console.log(\"123\")\r\n    if(page != 1){\r\n        removeAllChildNodes(mainContent)\r\n    }\r\n})\r\n\r\nfunction removeAllChildNodes(nodeList){\r\n    while (nodeList.firstChild){\r\n        nodeList.removeChild(nodeList.firstChild);\r\n    }\r\n}\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
